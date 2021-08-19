cc4a345ad92faf2f8acf68c3d61d31b0d0ee20a6
ThiHieuLUU <thihieuluu@gmail.com>
2021-08-19 22:16:36 +0200

    Check flake8

--- lettings/urls.py
+++ lettings/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "lettings"
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('lettings/', views.index, name='lettings_index'),
     path('lettings/<int:letting_id>/', views.letting, name='letting'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .models import Profile
 
-from django.http import HttpResponse
-from django.template import loader
 
 # Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero
 # pulvinar eget. Fusc faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum
